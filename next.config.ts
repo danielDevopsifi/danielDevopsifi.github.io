ceb19b9d83220f9e8778ecc37fcff7ca74682116
adildevopsifi <adil.waheed@devopsifi.com>
2025-05-22 09:34:53 +0500

    fixed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
     /* config options here */
+    basePath: isProduction ? '/' : undefined,
     output: 'export',
     trailingSlash: true,
     skipTrailingSlashRedirect: true,
